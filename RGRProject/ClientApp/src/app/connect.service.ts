d14ad5c4ebbe88d209883562baf930a246a96f48
eternallightz <eternallightz107@gmail.com>
2018-12-27 02:32:36 +0300

    add asynchronous requests with exponentional backoffs

--- RGRProject/ClientApp/src/app/connect.service.ts
+++ RGRProject/ClientApp/src/app/connect.service.ts
@@ -1,24 +1,66 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, pipe, from, of, range, timer, BehaviorSubject, } from 'rxjs';
+import { map, tap, retryWhen, delay, take, zip, mergeMap} from 'rxjs/operators';
+import { ajax } from 'rxjs/ajax';
+
 
 const BaseUrl = 'http://52.233.138.86:8097/api/v1';
+export const LOAD_INTERVAL_MS = 100; // 100 ms
+export const MAX_INTERVAL_MS = 2 * 1000; // 2 sec
+
 @Injectable()
 export class ConnectService {
-
+places: BehaviorSubject<any> = new BehaviorSubject({});
+place: BehaviorSubject<any> = new BehaviorSubject({});
+data = this.places.asObservable();
+currentPlace = this.place.asObservable();
 
 constructor(private http: HttpClient) { }
 
-  getPlaces(): Observable<any> {
-    return this.http.get<any>(`${BaseUrl}/GetPlaces`) 
+
+  backoff(maxTries, ms) {
+    return pipe(
+      retryWhen(attempts => range(1, maxTries)
+        .pipe(
+          zip(attempts, (i) => i),
+          map(i => i * i),
+          mergeMap(i =>  timer(i * ms))
+        )
+      )
+    )  
+  };
+
+  getPlaceProfile(id) {
+    ajax(`http://52.233.138.86:8097/api/v1/GetPlace/${id}`)
+    .pipe(
+      this.backoff(3, 250),
+    )
+    .subscribe(data => {
+      
+    });
   }
 
-  getPlaceProfile(id): Observable<any> {
-    return this.http.get<any>(`${BaseUrl}/GetPlace/${id}`) 
+
+
+  getPlaces(){
+      ajax('http://52.233.138.86:8097/api/v1/GetPlaces')
+      .pipe(
+        this.backoff(3, 250),
+      )
+      .subscribe((data: any) => {
+        console.log(data);
+        this.places.next(data.response.data);
+      });
+
   }
 
-  bookPlace(data): Observable<any> {
-    return this.http.post<any>(`${BaseUrl}/BookPlace`, {...data}) 
+  bookPlace(data) {
+    ajax({url: `${BaseUrl}/BookPlace`, body: data })
+      .pipe(this.backoff(3, 250))
+      .subscribe(data => {
+        return data;
+      });
   }
 
 }
--- RGRProject/ClientApp/src/app/home/home.component.ts
+++ RGRProject/ClientApp/src/app/home/home.component.ts
@@ -1,25 +1,36 @@
 import { ConnectService } from './../connect.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { Subscription, Observable, range, timer, pipe} from 'rxjs';
+import { map, tap, retryWhen, delay, take, zip, mergeMap} from 'rxjs/operators';
+import { ajax } from 'rxjs/ajax';
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
 })
 export class HomeComponent implements OnInit, OnDestroy {
-  getSub: Subscription;
+  places: any;
 
   constructor(private connectService: ConnectService){
 
   }
   ngOnInit(){
-    this.getSub =  this.connectService.getPlaces()
-      .subscribe(res => {
-        console.log(res);
-      });
+    this.connectService.getPlaces();
+    this.connectService.data
+    .subscribe(res => {
+      this.places = res;
+      console.log(this.places);         
+    });
+
   }
 
   ngOnDestroy(){
-    this.getSub.unsubscribe();
+    // this.getSub.unsubscribe();
   }
+
+
+  
+
+  
 }
